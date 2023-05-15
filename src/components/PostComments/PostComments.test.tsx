4804ed7cd3f501022f64777517247b6a210fba0d
Emely <emelly.dev@gmail.com>
2023-05-15 08:55:54 -0300

    Adicionando configuração para cobertura de testes

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -7,4 +7,24 @@ describe('Teste para o componente PostComment', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+
+    it('Adicionar comentários', () => {
+        render(<PostComment/>);
+
+        fireEvent.change(screen.getByTestId('textarea-comentario'), {
+            target: {
+                value: 'Primeiro Comentário',
+            }
+        });
+        fireEvent.click(screen.getByTestId('btn-comentario'));
+    
+        fireEvent.change(screen.getByTestId('textarea-comentario'), {
+            target: {
+                value: 'Segundo comentário',
+            }
+        });
+        fireEvent.click(screen.getByTestId('btn-comentario'));
+
+        expect(screen.getAllByTestId('elemento-comentario')).toHaveLength(2);
+    });
 });
